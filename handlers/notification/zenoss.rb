410fa5d5b346d3a420ed17fda788af5197f59b40
Miika Kankare <miika.kankare@cybercom.com>
2014-12-05 15:10:22 +0200

    Zenoss XML-RPC handler for Sensu

---
+++ handlers/notification/zenoss.rb
@@ -0,0 +1,84 @@
+#!/usr/bin/env ruby
+#
+# Sends events to Zenoss using XML-RPC.
+#
+# /etc/sensu/conf.d/zenoss_handler.json:
+# {
+#     "zenoss": {
+#         "server": "localhost",
+#         "port": 8081,
+#         "user": "admin",
+#         "password": "zenoss",
+#         "device": "hostname",
+#         "event_class": "/Status/Sensu"
+#     }
+# }
+#
+# Handler definitions:
+# {
+#     "handlers": {
+#         "zenoss": {
+#             "type": "pipe",
+#             "command": "/etc/sensu/handlers/zenoss.rb"
+#         }
+#     }
+# }
+#
+# 2014 / Miika Kankare / Cybercom Finland
+#
+
+require "rubygems" if RUBY_VERSION < "1.9.0"
+require "xmlrpc/client"
+require "sensu-handler"
+require "timeout"
+
+class Zenoss < Sensu::Handler
+    def status_to_severity
+      case @event['check']['status']
+          # Resolved / Clear
+          when 0
+              0
+          # Warning / Warning
+          when 1
+              3
+          # Critical / Critical
+          when 2
+              5
+          # Unknown to Critical
+          else
+              5
+      end
+    end
+
+
+    def create_zenoss_event
+        {
+          "device"     => settings["zenoss"]["device"],
+          "eventClass" => settings["zenoss"]["event_class"],
+          "component"  => @event["client"]["name"],
+          "summary"    => @event["check"]["output"],
+          "mesage"     => @event,
+          "severity"   => status_to_severity,
+
+        }
+    end
+
+
+    def handle
+        server = settings["zenoss"]["server"]
+        port = settings["zenoss"]["port"]
+        user = settings["zenoss"]["user"]
+        password = settings["zenoss"]["password"]
+
+        event = create_zenoss_event
+
+        begin
+            timeout(10) do
+                zenoss = XMLRPC::Client.new2("http://#{user}:#{password}@#{server}:#{port}")
+                zenoss.call("sendEvent", event)
+            end
+        rescue Timeout::Error
+            puts "Connection timed out when creating event: #{event[:client][:name]} - #{event[:check][:output]}"
+        end
+    end
+end
